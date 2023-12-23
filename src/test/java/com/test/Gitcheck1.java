d7871acfd49b7b7c122e64a7a1058f267d6af234
Rajhans <rajhansmehta1@gmail.com>
2023-12-23 18:38:37 +0530

    asian developer

---
+++ src/test/java/com/test/Gitcheck1.java
@@ -0,0 +1,17 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+public class Gitcheck1 {
+	
+
+	@Test
+	public void test() {
+		System.out.println("to check update");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("to check update second");
+	}
+}
